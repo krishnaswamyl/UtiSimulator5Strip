885fcae1d632046a5a58a32e8709c2840f03b94e
Krishnaswamy Loganathan <krishnaswamy@microlab.co.in>
2018-04-18 13:58:10 +0530

    Add project files.

---
+++ UtiSimulator/Form1.Populate.cs
@@ -0,0 +1,154 @@
+﻿
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+
+namespace UtiSimulator
+{
+   partial class Form1
+    {
+        private void populateTextBox()
+        {
+            P1_450nm = new List<TextBox>();
+            P1_450nm.Add(textBox1);
+            P1_450nm.Add(textBox2);
+            P1_450nm.Add(textBox3);
+            P1_450nm.Add(textBox4);
+            P1_450nm.Add(textBox5);
+            P1_450nm.Add(textBox6);
+            P1_450nm.Add(textBox7);
+            P1_450nm.Add(textBox8);
+
+            P1_630nm = new List<TextBox>();
+            P1_630nm.Add(textBox9);
+            P1_630nm.Add(textBox10);
+            P1_630nm.Add(textBox11);
+            P1_630nm.Add(textBox12);
+            P1_630nm.Add(textBox13);
+            P1_630nm.Add(textBox14);
+            P1_630nm.Add(textBox15);
+            P1_630nm.Add(textBox16);
+
+            P2_450nm = new List<TextBox>();
+            P2_450nm.Add(textBox17);
+            P2_450nm.Add(textBox18);
+            P2_450nm.Add(textBox19);
+            P2_450nm.Add(textBox20);
+            P2_450nm.Add(textBox21);
+            P2_450nm.Add(textBox22);
+            P2_450nm.Add(textBox23);
+            P2_450nm.Add(textBox24);
+
+            P2_630nm = new List<TextBox>();
+            P2_630nm.Add(textBox25);
+            P2_630nm.Add(textBox26);
+            P2_630nm.Add(textBox27);
+            P2_630nm.Add(textBox28);
+            P2_630nm.Add(textBox29);
+            P2_630nm.Add(textBox30);
+            P2_630nm.Add(textBox31);
+            P2_630nm.Add(textBox32);
+
+            P3_450nm = new List<TextBox>();
+            P3_450nm.Add(textBox33);
+            P3_450nm.Add(textBox34);
+            P3_450nm.Add(textBox35);
+            P3_450nm.Add(textBox36);
+            P3_450nm.Add(textBox37);
+            P3_450nm.Add(textBox38);
+            P3_450nm.Add(textBox39);
+            P3_450nm.Add(textBox40);
+
+            P3_630nm = new List<TextBox>();
+            P3_630nm.Add(textBox41);
+            P3_630nm.Add(textBox42);
+            P3_630nm.Add(textBox43);
+            P3_630nm.Add(textBox44);
+            P3_630nm.Add(textBox45);
+            P3_630nm.Add(textBox46);
+            P3_630nm.Add(textBox47);
+            P3_630nm.Add(textBox48);
+
+            P4_450nm = new List<TextBox>();
+            P4_450nm.Add(textBox49);
+            P4_450nm.Add(textBox50);
+            P4_450nm.Add(textBox51);
+            P4_450nm.Add(textBox52);
+            P4_450nm.Add(textBox53);
+            P4_450nm.Add(textBox54);
+            P4_450nm.Add(textBox55);
+            P4_450nm.Add(textBox56);
+
+            P4_630nm = new List<TextBox>();
+            P4_630nm.Add(textBox57);
+            P4_630nm.Add(textBox58);
+            P4_630nm.Add(textBox59);
+            P4_630nm.Add(textBox60);
+            P4_630nm.Add(textBox61);
+            P4_630nm.Add(textBox62);
+            P4_630nm.Add(textBox63);
+            P4_630nm.Add(textBox64);
+
+            P5_450nm = new List<TextBox>();
+            P5_450nm.Add(textBox65);
+            P5_450nm.Add(textBox66);
+            P5_450nm.Add(textBox67);
+            P5_450nm.Add(textBox68);
+            P5_450nm.Add(textBox69);
+            P5_450nm.Add(textBox70);
+            P5_450nm.Add(textBox71);
+            P5_450nm.Add(textBox72);
+
+            P5_630nm = new List<TextBox>();
+            P5_630nm.Add(textBox73);
+            P5_630nm.Add(textBox74);
+            P5_630nm.Add(textBox75);
+            P5_630nm.Add(textBox76);
+            P5_630nm.Add(textBox77);
+            P5_630nm.Add(textBox78);
+            P5_630nm.Add(textBox79);
+            P5_630nm.Add(textBox80);
+
+            P6_450nm = new List<TextBox>();
+            P6_450nm.Add(textBox81);
+            P6_450nm.Add(textBox82);
+            P6_450nm.Add(textBox83);
+            P6_450nm.Add(textBox84);
+            P6_450nm.Add(textBox85);
+            P6_450nm.Add(textBox86);
+            P6_450nm.Add(textBox87);
+            P6_450nm.Add(textBox88);
+
+            P6_630nm = new List<TextBox>();
+            P6_630nm.Add(textBox89);
+            P6_630nm.Add(textBox90);
+            P6_630nm.Add(textBox91);
+            P6_630nm.Add(textBox92);
+            P6_630nm.Add(textBox93);
+            P6_630nm.Add(textBox94);
+            P6_630nm.Add(textBox95);
+            P6_630nm.Add(textBox96);
+
+            P7_450nm = new List<TextBox>();
+            P7_450nm.Add(textBox97);
+            P7_450nm.Add(textBox98);
+            P7_450nm.Add(textBox99);
+            P7_450nm.Add(textBox100);
+            P7_450nm.Add(textBox101);
+            P7_450nm.Add(textBox102);
+            P7_450nm.Add(textBox103);
+            P7_450nm.Add(textBox104);
+
+            P7_630nm = new List<TextBox>();
+            P7_630nm.Add(textBox105);
+            P7_630nm.Add(textBox106);
+            P7_630nm.Add(textBox107);
+            P7_630nm.Add(textBox108);
+            P7_630nm.Add(textBox109);
+            P7_630nm.Add(textBox110);
+            P7_630nm.Add(textBox111);
+            P7_630nm.Add(textBox112);
+
+        }
+    }
+}
---
+++ UtiSimulator/Form1.cs
@@ -0,0 +1,592 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+
+namespace UtiSimulator
+{
+    public partial class Form1 : Form
+    {
+        static private SerialPort serp = new SerialPort();
+        static public bool read_timeout_flag = false;
+        private static Mutex mut = new Mutex();     //For Read Write command.
+        delegate void SetStatusBarTextCallback(string text);
+        private static EventWaitHandle adcSyc = new EventWaitHandle(true, EventResetMode.AutoReset);
+        public string comportname = "COM1";
+
+        private List<TextBox> P1_450nm;
+        private List<TextBox> P1_630nm;
+        private List<TextBox> P2_450nm;
+        private List<TextBox> P2_630nm;
+        private List<TextBox> P3_450nm;
+        private List<TextBox> P3_630nm;
+
+        private List<TextBox> P4_450nm;
+        private List<TextBox> P4_630nm;
+
+        private List<TextBox> P5_450nm;
+        private List<TextBox> P5_630nm;
+
+        private List<TextBox> P6_450nm;
+        private List<TextBox> P6_630nm;
+
+        private List<TextBox> P7_450nm;
+        private List<TextBox> P7_630nm;
+        private int no_of_strips = 3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SetText(string text)
+        {
+            if (this.statusStrip1.InvokeRequired)
+            {
+                SetStatusBarTextCallback d = new SetStatusBarTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.toolStripStatusLabel1.Text = text;
+            }
+        }
+
+        private void OnLoadForm(object sender, EventArgs e)
+        {
+            populateTextBox();
+            comboBox_COMPORT.BeginUpdate();
+            comboBox_COMPORT.Items.Clear();
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                comboBox_COMPORT.Items.Add(s);
+            }
+            string len = comboBox_COMPORT.Items.Count.ToString();
+            if (len.Equals("0"))
+            {
+                MessageBox.Show("No Comports detected on systems");
+                return;
+            }
+            comboBox_COMPORT.SelectedIndex = 0;
+            comboBox_COMPORT.EndUpdate();
+            
+
+
+        }
+
+
+
+        private string WriteAndReadCom(string com)
+        {
+            mut.WaitOne();
+            string temp = "NACK";
+            if (serp.IsOpen == false)
+            {
+                MessageBox.Show("Please connect to comm");
+                mut.ReleaseMutex();
+                read_timeout_flag = true;
+                return ("NACK");
+            }
+            serp.DiscardInBuffer();
+
+            serp.WriteLine(com);
+            read_timeout_flag = false;
+            try
+            {
+                temp = serp.ReadLine();
+            }
+            catch (System.TimeoutException)
+            {
+                mut.ReleaseMutex();
+                read_timeout_flag = true;
+                return ("Time Out");
+            }
+            SetText(temp);
+            mut.ReleaseMutex();
+            return (temp);
+        }
+
+        private void button_OpenPort_Click(object sender, EventArgs e)
+        {
+            if (button_OpenPort.Text.Equals("Open Port"))
+            {
+                string Comportname;
+                Comportname = comboBox_COMPORT.SelectedItem.ToString();
+                serp.PortName = Comportname;
+                serp.BaudRate = 115200;
+                serp.ReadBufferSize = 1024;
+                serp.NewLine = "\r\n";
+                try
+                {
+                    serp.Open();
+                }
+                catch (Exception se)
+                {
+                    MessageBox.Show(se.Message.ToString());
+                    return;
+                }
+                serp.ReadTimeout = 1000;
+                Thread.Sleep(100);
+                
+                WriteAndReadCom("10 ");
+                if (read_timeout_flag == false)
+                {
+                    button_OpenPort.Text = "Close Port";
+                    button_OpenPort.BackColor = Color.HotPink;
+
+                }
+                else
+                {
+                    serp.Dispose();
+                    serp.Close();
+                    SetText("No Communication");
+                }
+
+            }
+            else
+            {
+
+                serp.Dispose();
+                serp.Close();
+                button_OpenPort.Text = "Open Port";
+                button_OpenPort.BackColor = Color.YellowGreen;
+                
+            }
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            string temp;
+            int lp = 0, olp = 0;
+            List< TextBox > mylist= new List<TextBox>();
+
+            temp = "01 " + textBoxBaseLine.Text.ToString();
+            temp = WriteAndReadCom(temp);
+            if(temp.Equals("NACK"))
+            {
+                return;
+            }
+            temp = "02 " + textBoxNoStrips.Text.ToString();
+            temp = WriteAndReadCom(temp);
+            if (temp.Equals("NACK"))
+            {
+                return;
+            }
+            getNo_Of_Strips();
+            if (no_of_strips > 7 || no_of_strips < 3)
+            {
+                return;
+            }
+            for (olp = 0; olp < (no_of_strips*2); olp++)
+            {
+                temp = "03 " + (olp + 1);
+                temp = WriteAndReadCom(temp);
+                switch (olp)
+                {
+                    case 0:
+                        mylist = P1_450nm;
+                        break;
+                    case 1:
+                        mylist = P1_630nm;
+                        break;
+                    case 2:
+                        mylist = P2_450nm;
+                        break;
+                    case 3:
+                        mylist = P2_630nm;
+                        break;
+                    case 4:
+                        mylist = P3_450nm;
+                        break;
+                    case 5:
+                        mylist = P3_630nm;
+                        break;
+                    case 6:
+                        mylist = P4_450nm;
+                        break;
+                    case 7:
+                        mylist = P4_630nm;
+                        break;
+                    case 8:
+                        mylist = P5_450nm;
+                        break;
+                    case 9:
+                        mylist = P5_630nm;
+                        break;
+                    case 10:
+                        mylist = P6_450nm;
+                        break;
+                    case 11:
+                        mylist = P6_630nm;
+                        break;
+                    case 12:
+                        mylist = P7_450nm;
+                        break;
+                    case 13:
+                        mylist = P7_630nm;
+                        break;
+
+                };
+                for (lp = 0; lp < 8; lp++)
+                {
+                    
+                    temp = mylist[lp].Text.ToString();
+                    temp = WriteAndReadCom(temp);
+                    if (temp.Equals("NACK"))
+                    {
+                        return;
+                    }
+                }
+                SetText(" Strip " + (olp+1) + " Transmitted");                
+
+            }
+
+            return;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            string temp;
+            temp = WriteAndReadCom("04 ");
+            if(temp.Equals("Ack"))
+                {
+                SetText("Calcuations Done");
+
+            }
+            if (temp.Equals("NACK"))
+            {
+                SetText("Error System did not respond");
+                return;
+            }
+        }
+
+        private void buttonGetResults_Click(object sender, EventArgs e)
+        {
+            string temp;
+            richTextBox1.Clear();
+            int no_of_results = 16;
+            temp = WriteAndReadCom("11");
+            if (temp.Equals("NACK"))
+            {
+                SetText("Error System did not respond");
+                return;
+            }
+            try
+            {
+                no_of_results = int.Parse(temp);
+            }catch(Exception)
+            {
+                MessageBox.Show("No of Results returned from UTI card in not a number");
+                return;
+            }
+            for ( int k=0; k < no_of_results; k++)
+            {
+                temp = WriteAndReadCom("05 " + k);
+                if (temp.Equals("NACK"))
+                {
+                    SetText("Error System did not respond");
+                    return;
+                }
+                richTextBox1.AppendText(temp);
+                richTextBox1.AppendText("\r\n");
+
+            }
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            List<TextBox> mylistvf = new List<TextBox>();
+            getNo_Of_Strips();
+            if (no_of_strips > 7 || no_of_strips < 3)
+            {
+                return;
+            }
+            for (int olp = 0; olp < (no_of_strips*2); olp++)
+            {
+
+                switch (olp)
+                {
+                    case 0:
+                        mylistvf = P1_450nm;
+                        break;
+                    case 1:
+                        mylistvf = P1_630nm;
+                        break;
+                    case 2:
+                        mylistvf = P2_450nm;
+                        break;
+                    case 3:
+                        mylistvf = P2_630nm;
+                        break;
+                    case 4:
+                        mylistvf = P3_450nm;
+                        break;
+                    case 5:
+                        mylistvf = P3_630nm;
+                        break;
+                    case 6:
+                        mylistvf = P4_450nm;
+                        break;
+                    case 7:
+                        mylistvf = P4_630nm;
+                        break;
+                    case 8:
+                        mylistvf = P5_450nm;
+                        break;
+                    case 9:
+                        mylistvf = P5_630nm;
+                        break;
+                    case 10:
+                        mylistvf = P6_450nm;
+                        break;
+                    case 11:
+                        mylistvf = P6_630nm;
+                        break;
+                    case 12:
+                        mylistvf = P7_450nm;
+                        break;
+                    case 13:
+                        mylistvf = P7_630nm;
+                        break;
+
+                };
+                for (int lp = 0; lp < 8; lp++)
+                {
+                    mylistvf[lp].BackColor = Color.White;
+                }
+
+
+            }//loop ends here
+
+        }
+
+       
+        private void buttonVerify_Click(object sender, EventArgs e)
+        {
+            string temp, command;
+            double xt = 0.1, dt = 5.0;
+            int sx = 3, ds = 2;
+            List<TextBox> mylistvf = new List<TextBox>();
+
+            temp = WriteAndReadCom("06");
+            if (temp.Equals("NACK"))
+            {
+                return;
+            }
+            try
+            {
+                xt = double.Parse(temp);
+                dt = double.Parse(textBoxBaseLine.Text.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            if (xt == dt)
+            {
+                textBoxBaseLine.BackColor = Color.LightGreen;
+            }
+            else
+            {
+                textBoxBaseLine.BackColor = Color.Orange;
+            }
+
+            temp = WriteAndReadCom("07");
+            if (temp.Equals("NACK"))
+            {
+                return;
+            }
+            try
+            {
+                sx = int.Parse(temp);
+                ds = int.Parse(textBoxNoStrips.Text.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            if (sx == ds)
+            {
+                textBoxNoStrips.BackColor = Color.LightGreen;
+            }
+            else
+            {
+                textBoxNoStrips.BackColor = Color.Orange;
+            }
+            getNo_Of_Strips();
+            if (no_of_strips > 7 || no_of_strips < 3)
+            {
+                return;
+            }
+            for (int olp = 0; olp < (no_of_strips*2); olp++)
+            {
+                
+                switch (olp)
+                {
+                    case 0:
+                        mylistvf = P1_450nm;
+                        break;
+                    case 1:
+                        mylistvf = P1_630nm;
+                        break;
+                    case 2:
+                        mylistvf = P2_450nm;
+                        break;
+                    case 3:
+                        mylistvf = P2_630nm;
+                        break;
+                    case 4:
+                        mylistvf = P3_450nm;
+                        break;
+                    case 5:
+                        mylistvf = P3_630nm;
+                        break;
+                    case 6:
+                        mylistvf = P4_450nm;
+                        break;
+                    case 7:
+                        mylistvf = P4_630nm;
+                        break;
+                    case 8:
+                        mylistvf = P5_450nm;
+                        break;
+                    case 9:
+                        mylistvf = P5_630nm;
+                        break;
+                    case 10:
+                        mylistvf = P6_450nm;
+                        break;
+                    case 11:
+                        mylistvf = P6_630nm;
+                        break;
+                    case 12:
+                        mylistvf = P7_450nm;
+                        break;
+                    case 13:
+                        mylistvf = P7_630nm;
+                        break;
+
+                };
+                for (int lp = 0; lp < 8; lp++)
+                {
+                    command = String.Format("{0:2} {1:D2} {2:D2}","08",(olp+1),lp);
+                    temp = WriteAndReadCom(command);
+                    if (temp.Equals("NACK"))
+                    {
+                        return;
+                    }
+                    try
+                    {
+                        xt = double.Parse(temp);
+                        dt = double.Parse(mylistvf[lp].Text.ToString());
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                    }
+
+                    if(xt == dt)
+                    {
+                        mylistvf[lp].BackColor = Color.LightGreen;
+                    }
+                    else
+                    {
+                        mylistvf[lp].BackColor = Color.LightPink;
+                    }
+                  
+                }
+               
+
+            }//loop ends here
+        }
+        private void getNo_Of_Strips()
+        {
+            
+            try
+            {
+                no_of_strips = int.Parse(textBoxNoStrips.Text.ToString());
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("No of Strips not a number Please edit.."); return;
+            }
+            catch (ArgumentNullException)
+            {
+                MessageBox.Show("No of Strips not a number Please edit.."); return;
+            }
+            if (no_of_strips > 7 || no_of_strips < 3)
+            {
+                MessageBox.Show("Please enter a value for \nNo of Strips between 3 - 7");
+                return;
+            }
+        }
+        private void buttonPaste_Click(object sender, EventArgs e)
+        {
+            getNo_Of_Strips();
+            if (no_of_strips > 7 || no_of_strips < 3)
+            {
+                return;
+            }
+            string s = Clipboard.GetText();
+            string[] lines  = s.Split('\n');
+            string[] sCells = lines[0].Split('\t');            
+            int count = sCells.Length;
+            int i = 0, k = 0;
+            if(count < (no_of_strips*16))
+            {
+                MessageBox.Show(" Clip Board Data Length not Correct");
+                return;
+            }
+
+            for (i = 0; i < count; i += (no_of_strips * 2))
+            {
+                
+                P1_450nm[k].Text = sCells[i + 0];
+                P1_630nm[k].Text = sCells[i + 1];
+
+                P2_450nm[k].Text = sCells[i + 2];
+                P2_630nm[k].Text = sCells[i + 3];
+
+                P3_450nm[k].Text = sCells[i + 4];
+                P3_630nm[k].Text = sCells[i + 5];
+
+                if (no_of_strips == 3) { k++; continue; }
+
+                P4_450nm[k].Text = sCells[i + 6];
+                P4_630nm[k].Text = sCells[i + 7];
+
+                if (no_of_strips == 4) { k++; continue; }
+
+                P5_450nm[k].Text = sCells[i + 8];
+                P5_630nm[k].Text = sCells[i + 9];
+
+                if (no_of_strips == 5) { k++; continue; }
+
+                P6_450nm[k].Text = sCells[i + 10];
+                P6_630nm[k].Text = sCells[i + 11];
+
+                if (no_of_strips == 6) { k++; continue; }
+
+                P7_450nm[k].Text = sCells[i + 12];
+                P7_630nm[k].Text = sCells[i + 13];
+                k++;
+
+            }
+
+
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+
+}
